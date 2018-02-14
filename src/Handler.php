cc17e3418f9fb9d2fbe66ff145da227c7a880828
Christian Kaal <christiankaal@gmail.com>
2018-02-14 20:00:50 +0100

    Abstracted code and added docblocks

--- src/Handler.php
+++ src/Handler.php
@@ -17,6 +17,10 @@ class Handler extends ServiceProvider
      * @var bool
      */
     private $cacheEnabled;
+    /**
+     * @var string
+     */
+    private $contract;
 
     /**
      * @param string $contract
@@ -27,82 +31,93 @@ class Handler extends ServiceProvider
     public function decorate(string $contract, $chain)
     {
         $this->cacheEnabled = config('cache.enabled') ?? false;
+        $this->contract = $contract;
 
         $decoratedChain = $chain
-            ? $this->handlerFactory($contract, (array)$chain)
+            ? $this->handlerFactory((array)$chain)
             : [];
 
-        $this->app->singleton($contract, function () use ($contract, $decoratedChain) {
-            return Handler::handlerFactory(
-                $contract,
-                $decoratedChain
-            );
+        $this->app->singleton($contract, function () use ($decoratedChain) {
+            return Handler::handlerFactory($decoratedChain);
         });
     }
 
     /**
-     * @param string $contract
      * @param array  $chain
      *
      * @throws \Throwable
      *
      * @return mixed
      */
-    public function handlerFactory(string $contract, $chain)
+    public function handlerFactory($chain)
     {
         // Set the cache data if it is not set yet
         if ($this->cacheEnabled === null) {
             $this->cacheEnabled = config('cache.enabled') ?? false;
         }
 
-        return $this->processChain($contract, $chain);
+        return $this->processChain($chain);
     }
 
     /**
-     * @param string $contract
-     * @param        $chain
+     * @param $chain
      *
      * @throws \Throwable
      *
      * @return mixed
      */
-    private function processChain(string $contract, $chain)
+    private function processChain($chain)
     {
+        // Create a variable that will hold the instance
         $instance = null;
 
         foreach ((array)$chain as $parentClass => $class) {
+            // Check if cache is enabled and the class implements the cache class 
             if (!$this->cacheEnabled && get_parent_class($class) === AbstractCache::class) {
                 continue;
             }
 
-            $instance = $this->getDecoratedInstance($contract, $class, $instance);
+            /**
+             * Make sure the class implements the provided contract
+             * Throws an exception if the class does not implement the contract 
+             */
+            $this->assertClassImplementsContract($class);    
+
+            // Decorate the instance with the class
+            $instance = $this->getDecoratedInstance($class, $instance);
         }
 
+        // Return the instance
         return $instance;
     }
 
     /**
-     * @param string $contract
      * @param string $class
      * @param        $instance
      *
-     * @throws \Throwable
-     *
      * @return mixed
      */
-    private function getDecoratedInstance(string $contract, $class, $instance)
+    private function getDecoratedInstance($class, $instance)
+    {
+        return $instance
+            ? new $class($instance)
+            : new $class;
+    }
+
+    /**
+     * @param $class
+     *
+     * @throws \Throwable
+     */
+    private function assertClassImplementsContract($class)
     {
         $implementedInterfaces = class_implements($class);
 
         throw_unless(
-            isset($implementedInterfaces[$contract]),
+            isset($implementedInterfaces[$this->contract]),
             InterfaceNotImplementedException::class,
-            'Contract ' . $contract . ' is not implemented on ' . $class,
+            'Contract ' . $this->$contract . ' is not implemented on ' . $class,
             422
         );
-
-        return $instance
-            ? new $class($instance)
-            : new $class;
     }
 }

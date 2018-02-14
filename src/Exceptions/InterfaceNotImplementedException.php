46a22e587293878c8eb135013fa2a17ee2079e09
Christian <christian@freshapple.nl>
2018-02-14 11:15:57 +0100

    Added readme, updated composer.json, added exception and enforcing the implementation of a contract

---
+++ src/Exceptions/InterfaceNotImplementedException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CrixuAMG\Decorators\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class InterfaceNotImplementedException extends Exception
+{
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param  Request
+     *
+     * @return JsonResponse
+     */
+    public function render($request)
+    {
+        return response()->json([
+            'message' => $this->getMessage(),
+            'errors'  => [],
+        ], $this->getCode());
+    }
+}

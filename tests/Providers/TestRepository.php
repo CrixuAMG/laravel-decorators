1891ce0e0285305a2db7ebc0c478ef77bcda0247
Christian <christian@freshapple.nl>
2018-08-20 12:48:16 +0200

    Filled the test repository to allow for some simulated results

--- tests/Providers/TestRepository.php
+++ tests/Providers/TestRepository.php
@@ -38,6 +38,81 @@ class TestRepository extends AbstractRepository implements TestContract
      */
     public function store(array $data)
     {
-        return new TestModel();
+        return new $this->model;
+    }
+
+    /**
+     * @param bool $paginate
+     * @param int  $itemsPerPage
+     *
+     * @return LengthAwarePaginator|Collection|static[]
+     */
+    public function simpleIndex(bool $paginate = false, int $itemsPerPage = null)
+    {
+        return new Collection();
+    }
+
+    /**
+     * Create a new model
+     *
+     * @param array $data
+     *
+     * @return mixed
+     */
+    public function store(array $data)
+    {
+        return new $this->model;
+    }
+
+    /**
+     * @param array  $data
+     * @param string $createMethod
+     *
+     * @return Model
+     * @throws \Throwable
+     */
+    public function simpleStore(array $data, string $createMethod = 'create')
+    {
+        return new $this->model;
+    }
+
+    /**
+     * Update a model
+     *
+     * @param Model $model
+     * @param array $data
+     *
+     * @return mixed
+     */
+    public function update(Model $model, array $data)
+    {
+        return $model;
+    }
+
+    /**
+     * Return a single model
+     *
+     * @param Model $model
+     * @param mixed $relations
+     *
+     * @return mixed
+     */
+    public function show(Model $model, ...$relations)
+    {
+        // Return the model
+        return $model;
+    }
+
+    /**
+     * Delete a model
+     *
+     * @param Model $model
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function delete(Model $model)
+    {
+        return true;
     }
 }

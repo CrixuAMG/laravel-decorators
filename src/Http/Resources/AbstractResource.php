5ece6994e029a9ad1590be0c126d72683b507cec
CrixuAMG <christiankaal@gmail.com>
2020-06-08 10:32:00 +0200

    Update AbstractResource.php

--- src/Http/Resources/AbstractResource.php
+++ src/Http/Resources/AbstractResource.php
@@ -3,6 +3,8 @@
 namespace CrixuAMG\Decorators\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 /**
  * Class AbstractResource
@@ -14,31 +16,60 @@ class AbstractResource extends JsonResource
      * @param array $data
      * @return array
      */
-    public function format(array $data)
+    public function format(array $data, string $trimKey = '')
     {
+        $only = $this->getFilters($trimKey);
+
+        return $this->filterData($only, $data);
+    }
+
+    /**
+     * @param string $trimKey
+     * @return array
+     */
+    public function getFilters(string $trimKey): array
+    {
+        $only = [];
+
         if (!empty(request()->only)) {
             $only = json_decode(request()->only, true);
-
             if (!empty($only) && is_array($only)) {
-                foreach ($only as $keyToUnset) {
-                    $this->recursiveUnset($data, $keyToUnset);
+                foreach ($only as $index => $keyToUnset) {
+                    $only[$index] = Str::after($keyToUnset, $trimKey);
                 }
             }
         }
 
-        return $data;
+        return $only;
     }
 
     /**
-     * @param $array
-     * @param $unwanted_key
+     * @param array $only
+     * @param array $data
+     * @return array
      */
-    private function recursiveUnset(&$array, $unwanted_key) {
-        unset($array[$unwanted_key]);
-        foreach ($array as &$value) {
-            if (is_array($value)) {
-                $this->recursiveUnset($value, $unwanted_key);
+    public function filterData(array $only, array $data): array
+    {
+        if (!empty($only)) {
+            $newDataSet = [];
+
+            foreach ($data as $key => $value) {
+                $returnedValue = null;
+
+                if (is_array($value)) {
+                    $returnedValue = Arr::only($value, $only);
+                } else if (in_array($key, $only)) {
+                    $returnedValue = $value;
+                }
+
+                if ($returnedValue !== null) {
+                    $newDataSet[$key] = $returnedValue;
+                }
             }
+
+            $data = $newDataSet;
         }
+
+        return $data;
     }
 }

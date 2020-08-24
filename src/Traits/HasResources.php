f148ad0b9613d20714ae756f091086b99e998607
Christian Kaal <christiankaal@gmail.com>
2020-08-24 14:20:57 +0200

    Update AbstractResource.php Update AbstractResource.php Update HasResources.php Update AbstractController.php Removed old class
    Updated dependencies

--- src/Traits/HasResources.php
+++ src/Traits/HasResources.php
@@ -38,13 +38,40 @@ trait HasResources
     public function resourceful($data)
     {
         if ($this->resource) {
+            $resource = $this->getResource();
+
             if ($data instanceof LengthAwarePaginator || $data instanceof Collection) {
-                $data = $this->resource::collection($data);
+                $data = $resource::collection($data);
             } elseif ($data instanceof Model) {
-                $data = new $this->resource($data);
+                $data = new $resource($data);
             }
         }
 
         return $data;
+    }
+
+    /**
+     * @return mixed|string|null
+     */
+    public function getResource()
+    {
+        if (is_string($this->resource)) {
+            return $this->resource;
+        }
+
+        if (is_array($this->resource)) {
+            $requestedResource = request()->__resource ?? 'default';
+            $resource = null;
+
+            if (isset($this->resource[$requestedResource])) {
+               $resource = $this->resource[$requestedResource];
+            } elseif(isset($this->resource['default'])) {
+                $resource = $this->resource['default'];
+            }
+
+            abort_unless($resource, 422);
+        }
+
+        return $resource;
     }
 }
